f4482166816f87f1ee01bcfa44592552cc1826d3
Adeeb Mohammed <adeeb2358@gmail.com>
2019-12-08 16:11:38 +0900

    solution witrh out using recursion

---
+++ Binary_Search_Tree/_BST.h
@@ -0,0 +1,19 @@
+//
+//  _BST.h
+//  Binary_Search_Tree
+//
+//  Created by adeeb mohammed on 08/12/19.
+//  Copyright © 2019 adeeb mohammed. All rights reserved.
+//
+
+#ifndef _BST_h
+#define _BST_h
+/*
+    solution for binary search tree without recursion 
+ */
+
+class _BST{
+    
+};
+
+#endif /* _BST_h */
